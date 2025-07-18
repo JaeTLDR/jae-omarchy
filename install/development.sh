c8e0413ed63b6410c38cf380f8113fe2c79e5543
Jae <26292620+JaeTLDR@users.noreply.github.com>
2025-07-19 00:14:51 +1000

    Update development.sh

--- install/development.sh
+++ install/development.sh
@@ -1,6 +1,6 @@
+#mise imagemagick mariadb-libs postgresql-libs
 yay -S --noconfirm --needed \
-  cargo clang llvm mise \
-  imagemagick \
-  mariadb-libs postgresql-libs \
+  cargo clang llvm  \
+  go \
   github-cli \
   lazygit lazydocker-bin
