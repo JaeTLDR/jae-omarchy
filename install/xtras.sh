e95263baf7c214c1272504fed97cecaa762f5c4a
Jae <26292620+JaeTLDR@users.noreply.github.com>
2025-07-19 00:10:23 +1000

    Update xtras.sh

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,14 +1,15 @@
 if [ -z "$OMARCHY_BARE" ]; then
+# gnome-calculator libreoffice xournalpp localsend-bin
   yay -S --noconfirm --needed \
-    gnome-calculator gnome-keyring signal-desktop \
-    obsidian-bin libreoffice obs-studio kdenlive \
-    pinta xournalpp localsend-bin
+     gnome-keyring signal-desktop \
+    obsidian-bin  obs-studio kdenlive \
+    pinta  bitwarden
 
-  yay -S --noconfirm --needed typora ||
-    echo -e "\e[31mFailed to install Typora. Continuing without!\e[0m"
+  # yay -S --noconfirm --needed typora ||
+  #   echo -e "\e[31mFailed to install Typora. Continuing without!\e[0m"
 
-  yay -S --noconfirm --needed spotify dropbox-cli 1password-beta 1password-cli ||
-    echo -e "\e[31mFailed to install Spotify, Dropbox, and/or 1password because of a keyserver problem. Continuing without!\e[0m"
+  # yay -S --noconfirm --needed spotify dropbox-cli 1password-beta 1password-cli ||
+  #   echo -e "\e[31mFailed to install Spotify, Dropbox, and/or 1password because of a keyserver problem. Continuing without!\e[0m"
 fi
 
 # Copy over Omarchy applications
